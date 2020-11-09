20f0297e5bae7eb6c59c424495cc62512867d272
KevinEs14 <escobarkev@gmail.com>
2020-11-09 13:42:09 -0400

    creation of documents .xml of banckaccount,banktransaction and payment

--- src/main/java/bo/ucb/edu/ingsoft/bl/AgendaBl.java
+++ src/main/java/bo/ucb/edu/ingsoft/bl/AgendaBl.java
@@ -2,6 +2,8 @@ package bo.ucb.edu.ingsoft.bl;
 
 import bo.ucb.edu.ingsoft.dao.ContactDao;
 import bo.ucb.edu.ingsoft.dto.Contact;
+import bo.ucb.edu.ingsoft.models.BankAccount;
+import bo.ucb.edu.ingsoft.dao.BankAccountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,15 +11,22 @@ import org.springframework.stereotype.Service;
 public class AgendaBl {
     private ContactDao contactDao;
     private TransactionDao transactionDao;
+    private BankAccountDao bankAccountDao;
+    private BankTransactionDao bankTransactionDao;
+    private PaymentDao paymentDao;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ContactApi.class);
 
     @Autowired
-    public AgendaBl(ContactDao contactDao) {
+    public AgendaBl(ContactDao contactDao,TransactionDao transactionDao) {
         this.contactDao = contactDao;
+        this.transactionDao = transactionDao;
     }
 
     public Contact findContactById(Integer contactId) {
         return  contactDao.findByContactId(contactId);
+    }
+    public BankAccount createBankAccount(BankAccount bankAccount,Transaction transaction){
+        bankAccount.set
     }
 }
